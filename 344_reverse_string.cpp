33809fb05855f33e550d2e1e1c04436b5d066357
Om Modi <ocmodi2117@gmail.com>
2022-09-27 20:24:29 +0530

    file added

---
+++ 344_reverse_string.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseString(vector<char> &s)
+{
+    int n = s.size();
+    for (int i = 0, j = n - 1; i < j; i++, j--)
+        swap(s[i], s[j]);
+}
