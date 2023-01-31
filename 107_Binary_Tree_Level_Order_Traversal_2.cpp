79f8eb22a2456055476dcd42798b722385169402
Om Modi <ocmodi2117@gmail.com>
2023-01-31 21:13:04 +0530

    add file

---
+++ 107_Binary_Tree_Level_Order_Traversal_2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<vector<int>> levelOrderBottom(TreeNode *root)
+{
+    vector<vector<int>> ans;
+    if (!root)
+        return ans;
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int size = q.size();
+        vector<int> res;
+        for (int i = 0; i < size; i++)
+        {
+            TreeNode *temp = q.front();
+            q.pop();
+            if (temp->left)
+                q.push(temp->left);
+            if (temp->right)
+                q.push(temp->right);
+            res.push_back(temp->val);
+        }
+        ans.push_back(res);
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
