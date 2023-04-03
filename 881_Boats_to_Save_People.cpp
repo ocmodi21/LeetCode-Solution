d0e848795d0cb454feccd75067c971d54a7f2c8f
ocmodi21 <ocmodi2117@gmail.com>
2023-04-03 21:11:39 +0530

    add file

---
+++ 881_Boats_to_Save_People.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numRescueBoats(vector<int> &people, int limit)
+{
+    sort(people.begin(), people.end());
+    int minBoats = 0;
+    int i = 0, j = people.size() - 1;
+    while (i <= j)
+    {
+        minBoats++;
+        if (people[i] + people[j] <= limit)
+            i++;
+        j--;
+    }
+    return minBoats;
+}
