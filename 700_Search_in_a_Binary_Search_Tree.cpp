de2b0c3c1b0d06a366ba936b910d51718a6cd930
ocmodi21 <ocmodi2117@gmail.com>
2023-04-22 16:09:00 +0530

    add file

---
+++ 700_Search_in_a_Binary_Search_Tree.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *searchBST(TreeNode *root, int val)
+{
+    while (root != NULL && root->val != val)
+    {
+        root = val < root->val ? root->left : root->right;
+    }
+    return root;
+}
