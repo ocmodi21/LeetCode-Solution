25e8328e522cd1fd24fb7c79bc4b53c493ff9bd5
ocmodi21 <ocmodi2117@gmail.com>
2023-04-25 18:38:44 +0530

    add file

---
+++ 2336_Smallest_Number_in_Infinite_Set.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class SmallestInfiniteSet
+{
+public:
+    map<int, int> mp;
+    SmallestInfiniteSet()
+    {
+        for (int i = 1; i <= 1000; i++)
+            mp[i]++;
+    }
+
+    int popSmallest()
+    {
+        int temp = mp.begin()->first;
+        mp.erase(temp);
+        return temp;
+    }
+
+    void addBack(int num)
+    {
+        mp[num]++;
+    }
+};
