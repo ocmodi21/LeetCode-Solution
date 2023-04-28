760e701b0a2647b2c431def4e6ce0648d3e5ea49
ocmodi21 <ocmodi2117@gmail.com>
2023-04-28 20:11:44 +0530

    add file

---
+++ 678_Valid_Parenthesis_String.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkValidString(string s)
+{
+    int cmin = 0, cmax = 0;
+    for (char c : s)
+    {
+        if (c == '(')
+            cmax++, cmin++;
+        if (c == ')')
+            cmax--, cmin = max(cmin - 1, 0);
+        if (c == '*')
+            cmax++, cmin = max(cmin - 1, 0);
+        if (cmax < 0)
+            return false;
+    }
+    return cmin == 0;
+}
