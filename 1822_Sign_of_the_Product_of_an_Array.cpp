764cf779582c1dd66bb4f81b3b8e4b06e4fce684
ocmodi21 <ocmodi2117@gmail.com>
2023-05-02 14:16:43 +0530

    add file

---
+++ 1822_Sign_of_the_Product_of_an_Array.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arraySign(vector<int> &a)
+{
+    int cnt = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] == 0)
+            return 0;
+        if (a[i] < 0)
+            cnt++;
+    }
+    return cnt % 2 ? -1 : 1;
+}
