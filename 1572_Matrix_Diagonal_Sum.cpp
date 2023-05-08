954338ebd28abdbe9bde3075c7d98739f0b39aa5
Om Modi <ocmodi2117gmail.com>
2023-05-08 21:53:23 +0530

    add file

---
+++ 1572_Matrix_Diagonal_Sum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int diagonalSum(vector<vector<int>> &mat)
+{
+    int n = mat.size();
+    int primarySum = 0, secondarySum = 0;
+    for (int i = 0, j = 0; i < n; i++, j++)
+    {
+        primarySum += mat[i][j];
+        secondarySum += mat[i][n - 1 - j];
+    }
+    if (n % 2 == 1)
+    {
+        int mid = n / 2;
+        secondarySum -= mat[mid][mid];
+    }
+    return primarySum + secondarySum;
+}
