c4a2448d21d7167092c6788f90b9211d69af87bd
ocmodi21 <ocmodi2117@gmail.com>
2023-06-06 23:25:12 +0530

    update

--- 785_Is_Graph_Bipartite.cpp
+++ 785_Is_Graph_Bipartite.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// BFS
 bool bfs(int start, vector<int> &color, vector<vector<int>> &graph)
 {
 
@@ -38,4 +39,45 @@ bool isBipartite(vector<vector<int>> &graph)
     }
 
     return true;
+}
+
+// DFS
+
+bool solve(int s, int x, vector<int> &v, vector<vector<int>> &g)
+{
+    if (v[s] != -1)
+    {
+        if (v[s] != x)
+            return false;
+        return true;
+    }
+    v[s] = x;
+    bool ans = true;
+    for (auto &i : g[s])
+    {
+        ans &= solve(i, x ^ 1, v, g);
+    }
+    return ans;
+}
+bool isBipartite(vector<vector<int>> &graph)
+{
+    int n = graph.size(), i;
+    vector<int> v(n, -1);
+    vector<vector<int>> g(n);
+    for (i = 0; i < n; i++)
+    {
+        for (auto &j : graph[i])
+        {
+            g[i].push_back(j);
+        }
+    }
+    bool ans = true;
+    for (i = 0; i < n; i++)
+    {
+        if (v[i] == -1)
+        {
+            ans &= solve(i, 0, v, g);
+        }
+    }
+    return ans;
 }
