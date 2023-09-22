72bf61e2e771c8615eff1c4e96b110788e236bc7
ocmodi21 <ocmodi2117@gmail.com>
2023-09-22 21:03:50 +0530

    Two Pointer

---
+++ 392_Is_Subsequence.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isSubsequence(string s, string t)
+{
+    int i = 0, j = 0;
+    while (i < s.size() && j < t.size())
+    {
+        if (s[i] == t[j])
+        {
+            i++;
+        }
+        j++;
+    }
+    return i == s.size();
+}
